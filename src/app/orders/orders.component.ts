0ffb130f49f24b43affe6ee58045da256acbc0ce
ReneMurillo <rene.murillo95@hotmail.com>
2019-03-13 09:53:22 -0600

    Segunda parte

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../shared/order.service';
+import { Order } from '../shared/order.model';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class OrdersComponent implements OnInit {
+  orderList;
 
-  constructor() { }
+  constructor(private service: OrderService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.refreshlist();
   }
 
+  refreshlist(){
+    this.service.getOrderList().then(res => this.orderList = res);
+  }
+
+  openForEdit(orderID: number){
+    this.router.navigate(['/order/edit/'+orderID]);
+  }
+
+  onOrderDelete(id: number){
+    if(confirm('Are you sure to delete this record?')){
+      this.service.deleteOrder(id).then(res => {
+        this.refreshlist();
+        this.toastr.warning("Deleted Successfully", "Restaurant App");
+      });
+    }
+    
+  }
 }
